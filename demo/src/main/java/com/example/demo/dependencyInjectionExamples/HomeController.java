d43d26418da4447002253fb17c6312cb62e9d066
h1er <jesusgarciacarrasco99@gmail.com>
2025-12-05 18:22:16 +0100

    Minor changes

--- demo/src/main/java/com/example/demo/dependencyInjectionExamples/HomeController.java
+++ demo/src/main/java/com/example/demo/dependencyInjectionExamples/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demo.dependencyInjectionExamples;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -13,7 +14,7 @@ public class HomeController {
     @RequestMapping("/")
     public String index(){
 
-        System.out.println("value demo: "+propertiesValue);
+        //System.out.println("value demo: "+propertiesValue);
         return "index.html";
 
     }
